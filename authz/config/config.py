d8bed0d4d0e265c803197cf53eebb4b009f04357
gilani <rahelehgilani@gmail.com>
2022-02-07 07:49:12 +0100

    adding sqlalchemy structure

--- authz/config/config.py
+++ authz/config/config.py
@@ -7,3 +7,10 @@ class Config:
     DEBUG = bool(int(environ.get("THECHLAND_AUTHZ_DEBUG", "0")))
     TESTING = bool(int(environ.get("THECHLAND_AUTHZ_TESTING", "0")))
     SECRET_KEY = environ.get("THECHLAND_AUTHZ_SECRET_KEY", "HARD-HARD-HARD-SECRET-KEY")
+    
+    ################# Database config ##########################
+
+    SQLALCHEMY_DATABASE_URI = environ.get("THECHLAND_AUTHZ_DATABASE_URI", None)
+    SQLALCHEMY_ECHO = DEBUG
+    SQLALCHEMY_RECORD_QUERIES = DEBUG
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
